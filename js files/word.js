fe712c2d1f91598d709b926c7df23b0ec4274bcf
YBoah <yawboahene.yb@gmail.com>
2023-10-07 01:24:08 +0000

    Changed the words and added more words

--- js files/word.js
+++ js files/word.js
@@ -1,94 +1,210 @@
 let words = [
     {
-        word: "addition",
-        hint: "The process of adding numbers"
+        word: "fufu",
+        hint: "Traditional Ghanaian staple food made from cassava and plantains."
     },
     {
-        word: "meeting",
-        hint: "Event in which people come together"
+        word: "banku",
+        hint: "Fermented corn and cassava dough served with soup or sauce."
     },
     {
-        word: "number",
-        hint: "Math symbol used for counting"
+        word: "waakye",
+        hint: "A popular Ghanaian dish made from rice and beans."
     },
     {
-        word: "exchange",
-        hint: "The act of trading"
+        word: "jollof rice",
+        hint: "A delicious one-pot rice dish cooked with tomatoes, peppers, and spices."
     },
     {
-        word: "canvas",
-        hint: "Piece of fabric for oil painting"
+        word: "kenkey",
+        hint: "Fermented maize dumplings often served with fried fish and pepper sauce."
     },
     {
-        word: "garden",
-        hint: "Space for planting flower and plant"
+        word: "kelewele",
+        hint: "Spicy fried plantains, a popular Ghanaian street food."
     },
     {
-        word: "position",
-        hint: "Location of someone or something"
+        word: "red-red",
+        hint: "A hearty bean stew often served with fried plantains."
     },
     {
-        word: "feather",
-        hint: "Hair like outer covering of bird"
+        word: "tilapia",
+        hint: "A popular freshwater fish often grilled or fried."
     },
     {
-        word: "comfort",
-        hint: "A pleasant feeling of relaxation"
+        word: "groundnut soup",
+        hint: "A rich and nutty soup made with groundnuts (peanuts) and served with fufu or rice."
     },
     {
-        word: "tongue",
-        hint: "The muscular organ of mouth"
+        word: "ampesi",
+        hint: "A traditional Ghanaian dish made with yam, plantains, and cocoyam."
     },
     {
-        word: "expansion",
-        hint: "The process of increase or grow"
+        word: "okro soup",
+        hint: "A delicious soup made from okra and served with banku or fufu."
     },
     {
-        word: "country",
-        hint: "A politically identified region"
+        word: "sobolo",
+        hint: "A hibiscus flower drink often sweetened with ginger and served chilled."
     },
     {
-        word: "group",
-        hint: "A number of objects or persons"
+        word: "waakye leaves",
+        hint: "Leaves used to give waakye its distinctive color and flavor."
     },
     {
-        word: "taste",
-        hint: "Ability of tongue to detect flavour"
+        word: "shito",
+        hint: "A spicy black pepper sauce commonly used as a condiment."
     },
     {
-        word: "store",
-        hint: "Large shop where goods are traded"
+        word: "chichinga",
+        hint: "Spiced grilled meat skewers, a popular street food."
     },
     {
-        word: "field",
-        hint: "Area of land for farming activities"
+        word: "egg stew",
+        hint: "A tomato-based stew with hard-boiled eggs."
     },
     {
-        word: "friend",
-        hint: "Person other than a family member"
+        word: "gari",
+        hint: "Dried and grated cassava often eaten with groundnut soup or as a snack."
     },
     {
-        word: "pocket",
-        hint: "A bag for carrying small items"
+        word: "yam chips",
+        hint: "Thinly sliced yam deep-fried to make crispy chips."
     },
     {
-        word: "needle",
-        hint: "A thin and sharp metal pin"
+        word: "tilapia soup",
+        hint: "A flavorful soup made with tilapia fish and vegetables."
     },
     {
-        word: "expert",
-        hint: "Person with extensive knowledge"
+        word: "fante kenkey",
+        hint: "Variety of kenkey made by the Fante people, often served with fish."
     },
     {
-        word: "statement",
-        hint: "A declaration of something"
+        word: "light soup",
+        hint: "A spicy tomato-based soup often served with fufu or rice."
     },
     {
-        word: "second",
-        hint: "One-sixtieth of a minute"
+        word: "waakye stew",
+        hint: "A flavorful stew served with waakye, usually made with fish or meat."
     },
     {
-        word: "library",
-        hint: "Place containing collection of books"
+        word: "akple",
+        hint: "Togo and Volta Region delicacy made from corn dough and served with soup."
     },
-]
+    {
+        word: "abolo",
+        hint: "Fried bread made from fermented corn dough."
+    },
+    {
+        word: "yam porridge",
+        hint: "A thick yam-based soup often made with vegetables and fish."
+    },
+    {
+        word: "sankofa",
+        hint: "A popular Ghanaian herbal drink made from tree bark and roots."
+    },
+    {
+        word: "tilapia light soup",
+        hint: "A light and flavorful soup made with tilapia fish."
+    },
+    {
+        word: "omotuo",
+        hint: "Rice balls often served with groundnut or palm nut soup."
+    },
+    {
+        word: "kenkey and fried fish",
+        hint: "A classic Ghanaian combination of fermented maize dumplings and fried fish."
+    },
+    {
+        word: "akotonshi",
+        hint: "Spicy cowpea stew often served with fried plantains."
+    },
+    {
+        word: "kontomire stew",
+        hint: "Stew made with boiled taro leaves and often served with rice."
+    },
+    {
+        word: "kelewele and groundnut",
+        hint: "Spicy fried plantains served with groundnut sauce."
+    },
+    {
+        word: "fufu and light soup",
+        hint: "A classic Ghanaian dish combining fufu and a light tomato-based soup."
+    },
+    {
+        word: "aboboi",
+        hint: "A Ghanaian porridge made from maize, millet, or sorghum."
+    },
+    {
+        word: "asana",
+        hint: "Fried beans cake often served as a snack."
+    },
+    {
+        word: "nkate cake",
+        hint: "A sweet peanut candy made with roasted peanuts and sugar."
+    },
+    {
+        word: "asana and kenkey",
+        hint: "A popular Ghanaian combination of fried beans cake and kenkey."
+    },
+    {
+        word: "apapransa",
+        hint: "A spicy tomato-based soup often made with palm fruit extract."
+    },
+    {
+        word: "ame mo",
+        hint: "A Ghanaian dish made from yam and plantains often served with palm nut soup."
+    },
+    {
+        word: "abom",
+        hint: "A stew made with okra and often served with banku."
+    },
+    {
+        word: "fante fante",
+        hint: "A dish made from corn dough and palm nut soup, a Fante specialty."
+    },
+    {
+        word: "nkate kwan",
+        hint: "A Ghanaian groundnut soup often served with rice or fufu."
+    },
+    {
+        word: "domedo",
+        hint: "Spicy grilled pork often served with fried yam or plantains."
+    },
+    {
+        word: "wakye",
+        hint: "A popular Ghanaian dish made from rice and beans."
+    },
+    {
+        word: "abele",
+        hint: "A Ghanaian soup made with palm nut extract and often served with fufu."
+    },
+    {
+        word: "palm nut soup and fufu",
+        hint: "A classic Ghanaian combination of palm nut soup and fufu."
+    },
+    {
+        word: "tuo zaafi",
+        hint: "A northern Ghanaian dish made from millet or sorghum and served with soup."
+    },
+    {
+        word: "nkakra nkakra",
+        hint: "A Ghanaian dish made from corn dough and palm nut soup."
+    },
+    {
+        word: "abenkwan",
+        hint: "A rich and spicy groundnut soup often served with rice balls."
+    },
+    {
+        word: "fisherman's soup",
+        hint: "A spicy Ghanaian soup made with fish and palm nut extract."
+    },
+    {
+        word: "ademe",
+        hint: "A Ghanaian porridge made from corn dough and palm nut extract."
+    },
+    {
+        word: "mole stew",
+        hint: "A popular northern Ghanaian stew made with groundnut paste."
+    },
+];
